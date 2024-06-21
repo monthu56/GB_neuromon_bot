61c2dbc0e6367bb2895204733d820062290f7b23
month <monthu56@mail.ru>
2024-06-21 19:04:02 +1100

    Test

--- main.py
+++ main.py
@@ -4,35 +4,66 @@ import config
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.utils.formatting import Text, Bold
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import json
+import aiohttp
 
 API_TOKEN = config.token
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    ))
 dp = Dispatcher()
 
 
 @dp.message(Command(commands=['start']))
 async def send_welcome(message: Message):
-    await message.answer("Привет! Я эхобот. Пришли мне сообщение, и я его повторю.")
+    await message.answer(f"Привет, <b>{message.from_user.first_name}</b>! \n"
+                         f"Я эхобот. Пришли мне сообщение, и я его повторю. \n"
+                         f"Для тебя есть несколько доступных команд:\n"
+                         f"/help - вывод справочной информации\n"
+                         f"/shop - Каталог товаров"
+                         f"\n/echo - Повторяет сообщение\n"
+                         )
+@dp.callback_query(F.data == "help")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Я рад тебе помочь!")
+    await callback.answer()
 
 
 @dp.message(Command(commands=['help']))
 async def command1(message: types.Message):
-    await message.answer("Тут какой-то поясняющий текст")
-
-
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Мне помогло!",
+        callback_data="help"),
+   await message.answer(f"Тут какой-то поясняющий текст, <b>{message.from_user.username}</b>", reply_markup=builder.as_markup())
+    )
+@dp.callback_query(F.data == "echo")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Бла-бла-бла-бла-бла-бла-бла-бла")
+    await callback.answer()
 @dp.message(Command(commands=['shop']))
 async def command2(message: types.Message):
     await message.answer("Типа список товаров")
 
+@dp.message(Command(commands=['echo']))
+async def command3(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Повторить",
+        callback_data="echo")
+    )
+    await message.answer("Типа повторяющее сообщение", reply_markup=builder.as_markup())
 
 @dp.message(F.text)
 async def echo(message: types.Message):
     await message.answer(message.text)
-
-#kjasfvkj
 
 async def main():
     await dp.start_polling(bot)
