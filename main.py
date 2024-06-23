35af5abee4d2caabfe1a63e33018e97d45163597
month <monthu56@mail.ru>
2024-06-23 14:29:39 +1100

    Работающий бот

--- main.py
+++ main.py
@@ -3,76 +3,101 @@ import asyncio
 import config
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.client.default import DefaultBotProperties
 
+# Конфигурация токена
 API_TOKEN = config.token
 
-logging.basicConfig(level=logging.INFO)
+# Настройка логирования
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-bot = Bot(token=API_TOKEN, default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML
-    ))
+logger = logging.getLogger(__name__)
+
+# Создание бота и диспетчера
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
-
+# Обработчик команды /start
 @dp.message(Command(commands=['start']))
 async def send_welcome(message: Message):
-    await message.answer(f"Привет, <b>{message.from_user.first_name}</b>! \n"
-                         f"Я эхобот. Пришли мне сообщение, и я его повторю. \n"
-                         f"Для тебя есть несколько доступных команд:\n"
-                         f"/help - вывод справочной информации\n"
-                         f"/shop - Каталог товаров"
-                         f"\n/echo - Повторяет сообщение\n"
-                         )
-@dp.callback_query(F.data == "help")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Я рад тебе помочь!")
-    await callback.answer()
-
-
-@dp.message(Command(commands=['help']))
-async def command1(message: types.Message):
-    builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(
-        text="Мне помогло!",
-        callback_data="help"),
-   await message.answer(f"Тут какой-то поясняющий текст, <b>{message.from_user.username}</b>", reply_markup=builder.as_markup())
+    logger.info(f"Получено сообщение от {message.from_user.username}: {message.text}")
+    await message.answer(
+        f"Привет, <b>{message.from_user.first_name}</b>! \n"
+        "Я эхобот. Пришли мне сообщение, и я его повторю. \n"
+        "Для тебя есть несколько доступных команд:\n"
+        "/help - вывод справочной информации\n"
+        "/shop - Каталог товаров\n"
+        "/echo - Повторяет сообщение\n"
     )
-@dp.callback_query(F.data == "echo")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Бла-бла-бла-бла-бла-бла-бла-бла")
-    await callback.answer()
-@dp.message(Command(commands=['shop']))
-async def command2(message: types.Message):
+
+# Обработчик команды /help
+@dp.message(Command(commands=['help']))
+async def command_help(message: types.Message):
+    logger.info(f"Получено сообщение от {message.from_user.username}: {message.text}")
     builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(text="Магазин", callback_data="shop")),
-
-    await message.answer("Типа список товаров", reply_markup=builder.as_markup())
-
-@dp.callback_query(F.data  ==  "shop")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Перечень товаров магазина")
+    builder.add(
+        types.InlineKeyboardButton(
+            text="Мне помогло!",
+            callback_data="help"
+        )
+    )
+    await message.answer(
+        f"Тут какой-то поясняющий текст, <b>{message.from_user.username}</b>",
+        reply_markup=builder.as_markup()
+    )
+# Обработчик callback для "help"
+@dp.callback_query(F.data == "help")
+async def help_callback(callback: CallbackQuery):
+    logger.info(f"Пользователь {callback.from_user.username} нажал кнопку 'Мне помогло!'")
+    await callback.message.answer("Рад, что помог!")
     await callback.answer()
-@dp.callback_query(F.data  ==  "shop")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.message.answer("Перечень товаров магазина")
+
+# Обработчик команды /shop
+@dp.message(Command(commands=['shop']))
+async def command_shop(message: types.Message):
+    logger.info(f"Получено сообщение от {message.from_user.username}: {message.text}")
+    builder = InlineKeyboardBuilder()
+    builder.add(
+        types.InlineKeyboardButton(
+            text="Товар 1",
+            callback_data="item_1"
+        ),
+        types.InlineKeyboardButton(
+            text="Товар 2",
+            callback_data="item_2"
+        )
+    )
+    await message.answer(
+        "Каталог товаров:",
+        reply_markup=builder.as_markup()
+    )
+
+# Обработчики для товаров
+@dp.callback_query(F.data == "item_1")
+async def item_1(callback: CallbackQuery):
+    logger.info(f"Пользователь {callback.from_user.username} выбрал Товар 1")
+    await callback.message.answer("Описание Товара 1")
     await callback.answer()
-@dp.message(F.text)
+
+@dp.callback_query(F.data == "item_2")
+async def item_2(callback: CallbackQuery):
+    logger.info(f"Пользователь {callback.from_user.username} выбрал Товар 2")
+    await callback.message.answer("Описание Товара 2")
+    await callback.answer()
+
+# Обработчик текстовых сообщений
+@dp.message()
 async def echo(message: types.Message):
+    logger.info(f"Получено сообщение от {message.from_user.username}: {message.text}")
     await message.answer(message.text)
-@dp.message(Command(commands=['info']))
-async def command4(message: types.Message):
-    await message.answer("Какая-то информация"
-                         "Ща мы напишем что-нибудь более интересное")
-@dp.callback_query(F.data == "info")
-async def send_info(message: types.Message):
-    await message.answer()
+
+# Функция запуска polling
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     asyncio.run(main())
